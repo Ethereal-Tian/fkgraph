d8dc7463a07b9f30821f100df815dff81265cf27
Ethereal-Tian <zxzx20112011@163.com>
2022-07-15 17:10:00 +0800

    add core

--- voxblox/voxblox/include/voxblox/alignment/icp.h
+++ voxblox/voxblox/include/voxblox/alignment/icp.h
@@ -1,7 +1,7 @@
 /*
  * Software License Agreement (BSD License)
  *
- *  Point Cloud Library (PCL) - www.pointclouds.org
+ *  Point Cloud Library (PCL) - www.pointclouds.org 
  *  Copyright (c) 2010, Willow Garage, Inc.
  *  Copyright (c) 2012-, Open Perception, Inc.
  *
